9333025e6a428314079c8fea2a9b966b036ff93d
jccracho <87005884+jccracho@users.noreply.github.com>
2022-06-04 21:49:53 +0800

    Delete login.js

--- login.js
+++
@@ -1,6 +0,0 @@
-const login = require("facebook-chat-api");
-
-login({email: "Jibes Miserte", password: "09368924417"}, (err, api) => {
-    if(err) return console.error(err);
-    // Here you can use the api
-});
